a82d7b440fdc09b83a3bf5159720ae1d37a6ee78
Vikas <vy348697@gmail.com>
2024-04-04 23:47:55 +0530

    database connected

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+import { exit } from 'node:process';
+import { error } from "node:console";
+const connectDB = async()=>{
+try {
+    const connectionDatabase = await mongoose.connect(`${process.env.MANGOBD_URL}/${DB_NAME}`)
+    console.log("MongoBD connected succesfully " , (connectionDatabase.connection.host));
+    throw error
+} catch (error) {
+    console.log("Failed to connect database : " ,error)
+    process.exit(1)
+}
+}
+
+export default connectDB;
